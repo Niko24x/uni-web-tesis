9def081fc7989b11a54dd25c4e44d81e46569d56
niko24x <nikolascohn@gmail.com>
2019-10-31 19:23:17 -0600

    se agrego documentacion

--- uniweb/maps/views.py
+++ uniweb/maps/views.py
@@ -3,11 +3,34 @@ from itertools import chain
 from core.models import Avistamientom, Camara, Finca, Camara, Avistamientom
 
 def default_map(request):
+
+    class itemCamara(object):
+        def __init__(self, ids, nombre_camara, ubicacion_latitud, ubicacion_longitud, listadoAvistamientos=0):
+            self.id = ids
+            self.nombre_camara = nombre_camara
+            self.ubicacion_latitud = ubicacion_latitud
+            self.ubicacion_longitud  = ubicacion_longitud
+            self.listadoAvistamientos = listadoAvistamientos
+
     listaAvistamiento = Avistamientom.objects.all()
+
     cantFincas = Finca.objects.count()
     cantCamaras = Camara.objects.count()
     cantAvistamiento = Avistamientom.objects.count()
-    return render(request, 'base/maps/default.html', { 
-            'consulta': listaAvistamiento, 'firstValue': listaAvistamiento[0],  'cantFincas': cantFincas,
-            'cantCamaras': cantCamaras, 'cantAvistamiento': cantAvistamiento
+
+    listaCamaras = Camara.objects.all()
+    listaItemCamara = []
+    for cam in listaCamaras:
+        avistamientoResult = Avistamientom.objects.filter(camara_fk=cam.id)
+        listaItemCamara.append(itemCamara(cam.id, cam.nombre_camara, cam.ubicacion_latitud, cam.ubicacion_longitud, avistamientoResult))
+
+    for cami in listaItemCamara:
+        print(cami.id)
+        print(cami.nombre_camara)
+        print(cami.listadoAvistamientos)
+        print("----------------------")
+
+    return render(request, 'base/maps/defaultclick.html', { 
+            'consulta': listaAvistamiento, 'firstValue': listaItemCamara[0],  'cantFincas': cantFincas,
+            'cantCamaras': cantCamaras, 'cantAvistamiento': cantAvistamiento, 'listaItemCamara': listaItemCamara
         })
